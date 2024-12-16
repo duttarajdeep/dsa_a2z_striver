1c86fddca1f1891e465aff4be743245a21c5cf16
Rajdeep Dutta <rajdeep.dutta.5007@gmail.com>
2024-12-16 17:30:01 +0530

    DSA

--- src/step3/hard/OverlappingIntervals.java
+++ src/step3/hard/OverlappingIntervals.java
@@ -10,7 +10,7 @@ public class OverlappingIntervals {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] intervals = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
-		int[][] output = merge(intervals);
+		merge(intervals);
 
 	}
 
