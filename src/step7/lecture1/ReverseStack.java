7845e9dcc731142bffaf5782cd7f3ae475e84b10
Rajdeep Dutta <rajdeep.dutta.5007@gmail.com>
2025-01-15 23:39:21 +0530

    DSA

---
+++ src/step7/lecture1/ReverseStack.java
@@ -0,0 +1,41 @@
+package step7.lecture1;
+
+import java.util.Stack;
+
+public class ReverseStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> input = new Stack<>();
+		input.push(1);
+		input.push(2);
+		input.push(3);
+		reverse(input);
+
+		while (!input.isEmpty()) {
+			int top = input.pop();
+			System.out.println(top + " ");
+		}
+
+	}
+
+	static void reverse(Stack<Integer> stack) {
+		if (stack.size() == 1)
+			return;
+		int top = stack.pop();
+		reverse(stack);
+		insert(stack, top);
+	}
+
+	static void insert(Stack<Integer> stack, int temp) {
+		if (stack.size() == 0) {
+			stack.push(temp);
+			return;
+		}
+
+		int top = stack.pop();
+		insert(stack, temp);
+		stack.push(top);
+	}
+
+}
