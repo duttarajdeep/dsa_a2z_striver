872e74a2cddefa0855794f502b3722823d3f470c
Rajdeep Dutta <rajdeep.dutta.5007@gmail.com>
2025-01-16 23:02:33 +0530

    DSA

---
+++ src/step7/lecture1/KthGrammar.java
@@ -0,0 +1,20 @@
+package step7.lecture1;
+
+public class KthGrammar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(solve(2, 2));
+	}
+
+	static int solve(int n, int k) {
+		if (n == 1 && k == 1)
+			return 0;
+		int mid = (int) ((Math.pow(2, n - 1)) / 2);
+		if (k <= mid)
+			return solve(n - 1, k);
+		else
+			return 1 - solve(n - 1, k - mid);
+	}
+
+}
---
+++ src/step7/lecture1/MinimumWatchScore.java
@@ -0,0 +1,42 @@
+package step7.lecture1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinimumWatchScore {
+
+	public static int findMinimumWatchScore(int[] watchHistory, int series1, int series2) {
+		int n = watchHistory.length;
+		int minScore = Integer.MAX_VALUE;
+
+		int left = 0;
+		Map<Integer, Integer> seriesCount = new HashMap<>();
+
+		for (int right = 0; right < n; right++) {
+			seriesCount.put(watchHistory[right], seriesCount.getOrDefault(watchHistory[right], 0) + 1);
+
+			while (seriesCount.containsKey(series1) && seriesCount.containsKey(series2)) {
+
+				int currentScore = right - left + 1;
+				minScore = Math.min(minScore, currentScore);
+
+				seriesCount.put(watchHistory[left], seriesCount.get(watchHistory[left]) - 1);
+				if (seriesCount.get(watchHistory[left]) == 0) {
+					seriesCount.remove(watchHistory[left]);
+				}
+				left++;
+			}
+		}
+
+		return minScore == Integer.MAX_VALUE ? -1 : minScore;
+	}
+
+	public static void main(String[] args) {
+		int[] watchHistory = { 1, 3, 2, 1, 4 };
+		int series1 = 1;
+		int series2 = 2;
+
+		int result = findMinimumWatchScore(watchHistory, series1, series2);
+		System.out.println("Minimum Watch Score: " + result); // Output: 2
+	}
+}
